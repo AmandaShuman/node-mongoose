0ff5e373a124a1165a9ce8db7d2ac9a81939fdab
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-05-20 11:20:28 -0400

    Mongoose Part 3

--- index.js
+++ index.js
@@ -4,24 +4,41 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite'; //connect us to campsitedb in mongodb server 
 const connect = mongoose.connect(url, { //next 3 options deal with handling deprecation warnings from mongodb node driver
   useCreateIndex: true,
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
-}); //connect wrapper around mongodb node drivers connect method (like connecting with Mongo.client using mongoose)
+}) //connect wrapper around mongodb node drivers connect method (like connecting with Mongo.client using mongoose)
 
 connect.then(() => {
 
   console.log('Connected correctly to server');
 
   Campsite.create({ //creates new document based on mongoose model
-    name: 'React Lake Campground',
+    name: 'React Lake Campgrounds',
     description: 'test'
   }) //auto-saves now 
   .then(campsite => {
-    console.log(campsite);
-    return Campsite.find();
+    console.log(campsite); //will show original doc
+
+    return Campsite.findByIdAndUpdate(campsite._id, {
+      $set: { description: 'Updated Test Document' }
+    }, {
+      new: true //will cause method to return updated document; otherwise, we would get the original before the update
+    });
   })
-  .then(campsites => { 
-    console.log(campsites);
+  .then(campsite => {
+    console.log(campsite); //will show doc with updated description
+
+    campsite.comments.push({ //add comments subdoc
+      rating: 5,
+      text: 'What a magnificent view!',
+      author: 'Tinus Lorvaldes'
+    });
+
+    return campsite.save(); //need in order for subdoc update to take effect
+  })
+  .then(campsite => { 
+    console.log(campsite); //now just getting single campsite doc rather than an array of campsites
     return Campsite.deleteMany(); //delete all docs created from campsite model
   })
   .then(() => {
--- models/campsite.js
+++ models/campsite.js
@@ -2,6 +2,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({ //for storing commnts about a campsite
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
 //schema
 const campsiteSchema = new Schema({
   name: { //1st argument is required
@@ -12,7 +31,8 @@ const campsiteSchema = new Schema({
   description: {
     type: String,
     required: true
-  }
+  },
+  comments: [commentSchema] //this will cause every campsite doc to be able to contain mult. comment docs stored w/in an array
 }, { //2nd optional argument for configuration options
   timestamps: true, //will cause Mongoose to auto add 2 props to schema called createdAt and updatedAt
 });
